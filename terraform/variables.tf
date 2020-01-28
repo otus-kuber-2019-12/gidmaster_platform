0740d3d2926c3086d05788b9d965997f8a27756f
Gidmaster <k.syrovatsky@gmail.com>
2020-01-29 02:02:19 +0300

    [HW06] Kubernetes-templating

---
+++ terraform/variables.tf
@@ -0,0 +1,33 @@
+variable "gcp_project_id" {
+  description = "project ID"
+}
+
+variable "gcp_location" {
+  description = "region name"
+  default     = "europe-west1"
+}
+
+variable "gcp_cluster_name" {
+  description = "k8s Cluster name"
+}
+
+variable "daily_maintenance_window_start_time" {
+  description = ""
+  default = "04:00"
+}
+
+variable "gcp_pool_name" {
+  description = "name of kubernetes node pool"
+}
+
+variable "worker_disk_size" {
+  description = ""  
+}
+
+variable "worker_machine_type" {
+  description = ""
+}
+
+variable "address_name" {
+  description = "name of address pool"
+}
